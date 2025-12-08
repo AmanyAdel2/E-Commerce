fd01f61430c63ca2efa8095186648cb312acd1b3
AmanyAdel2 <amanyadel20242024@gmail.com>
2025-12-08 15:22:46 +0200

    Initial Commit

---
+++ e-Commerce/e_CommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_CommerceApp.swift
+//  e-Commerce
+//
+//  Created by Macos on 08/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct e_CommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
